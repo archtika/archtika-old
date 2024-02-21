33fa02dc3d278c7cd377967246f9a0785b0f2a6d
thiloho <123883702+thiloho@users.noreply.github.com>
2024-02-21 21:14:10 +0100

    Remove unnecessary backend files

--- backend/rest-api/js-output/db-connector.js
+++
@@ -1,8 +0,0 @@
-import fastifyPlugin from 'fastify-plugin';
-import fastifyPostgres from '@fastify/postgres';
-async function dbConnector(fastify) {
-    fastify.register(fastifyPostgres, {
-        connectionString: 'postgres://postgres:password@localhost:5432/postgres'
-    });
-}
-export default fastifyPlugin(dbConnector);
