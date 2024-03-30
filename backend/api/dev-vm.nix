ef90a376b5fb074327048ff9b37cca783e6c9c66
thiloho <123883702+thiloho@users.noreply.github.com>
2024-03-30 13:47:46 +0100

    Add minio and redis to dev vm

--- backend/api/dev-vm.nix
+++ backend/api/dev-vm.nix
@@ -3,23 +3,44 @@
 
   networking.hostName = "archtika";
 
+  nix.settings.experimental-features = [ "nix-command flakes" ];
+
   users.users.dev = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     password = "dev";
   };
 
-  virtualisation.forwardPorts = [{
-    from = "host";
-    host.port = 15432;
-    guest.port = 5432;
-  }];
-
-  virtualisation.graphics = false;
+  virtualisation = {
+    graphics = false;
+    memorySize = 2048;
+    forwardPorts = [
+      {
+        from = "host";
+        host.port = 15432;
+        guest.port = 5432;
+      }
+      {
+        from = "host";
+        host.port = 16379;
+        guest.port = 6379;
+      }
+      {
+        from = "host";
+        host.port = 19000;
+        guest.port = 9000;
+      }
+      {
+        from = "host";
+        host.port = 19001;
+        guest.port = 9001;
+      }
+    ];
+  };
 
   networking.firewall = {
-    allowedTCPPorts = [ 5432 ];
-    allowedUDPPorts = [ 5432 ];
+    allowedTCPPorts = [ 5432 9000 9001 ];
+    allowedUDPPorts = [ 5432 9000 9001 ];
   };
 
   services = {
@@ -32,6 +53,20 @@
         host all all all trust
       '';
       enableTCPIP = true;
+    };
+    redis.servers = {
+      archtika = {
+        enable = true;
+        bind = null;
+        requirePass = "dev";
+        port = 6379;
+        openFirewall = true;
+      };
+    };
+    minio = {
+      enable = true;
+      listenAddress = "0.0.0.0:9000";
+      consoleAddress = "0.0.0.0:9001";
     };
   };
 
